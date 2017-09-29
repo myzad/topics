a792dca3f0959000e257c4267c07c536f93cbfe1
myzad <myriszadip@gmail.com>
2017-09-29 23:40:24 +0200

    first files

---
+++ header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Share your opinion</title>
+        <link rel="stylesheet" type="text/css" href="vue/style.css">
+    </head>
+        
+    <body>
